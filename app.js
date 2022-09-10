1b2f1872f74a82250d0ddb9f6934229a8d43e0d6
Jesse <jessean.infiltrator@gmail.com>
2022-09-10 16:23:41 -0700

    Part 1 Code Only

---
+++ app.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const connectDB = require('./config/db');
+
+//Load config
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+// Logging
+//!Add the word .engine after expbhs
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine(
+    {
+        defaultLayout: 'main',
+        extname: '.hbs'
+    }
+));
+app.set('view engine', '.hbs');
+
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+// TIMESTAMP BOOKMARK 05:18:43
+
+const PORT = process.env.PORT || 8500;
+
+app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on ${PORT}`));
