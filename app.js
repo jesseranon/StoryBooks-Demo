0729b1a6236701a5006022f42404eb724f0b1969
Jesse <jessean.infiltrator@gmail.com>
2022-09-12 18:08:17 -0700

    added helpers and editing capability

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -22,17 +23,36 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
 // Logging
 //!Add the word .engine after expbhs
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
+
 // Handlebars
 app.engine('.hbs', exphbs.engine(
     {
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select
+        },
         defaultLayout: 'main',
-        extname: '.hbs'
+        extname: '.hbs',
     }
 ));
 app.set('view engine', '.hbs');
@@ -53,6 +73,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -61,7 +87,7 @@ app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 app.use('/stories', require('./routes/stories'));
 
-// TIMESTAMP BOOKMARK P2 ~00:26:00
+// TIMESTAMP BOOKMARK P2 ~05:42:00
 
 const PORT = process.env.PORT || 8500;
 
