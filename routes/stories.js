b8b3396cfa80160b2e72c4bf9a635da668814634
Jesse <jessean.infiltrator@gmail.com>
2022-09-13 18:43:36 -0700

    added capability to view a single story

--- routes/stories.js
+++ routes/stories.js
@@ -52,6 +52,32 @@ router.get(
     }
 );
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get(
+    '/:id',
+    ensureAuth,
+    async (req, res) => {
+        try {
+            let story = await Story.findById(req.params.id)
+                .populate('user')
+                .lean();
+
+            if (!story) {
+                return res.render('error/404');
+            }
+
+            res.render('stories/show', {
+                story
+            });
+        } catch (err) {
+            console.error(err);
+            return res.render('error/404');
+        }
+        
+    }
+);
+
 // @desc    Show edit page
 // @route   GET /stories/add
 router.get(
@@ -82,20 +108,66 @@ router.put(
     '/:id',
     ensureAuth,
     async (req, res) => {
-        let story = await Story.findById(req.params.id).lean();
+        try {
+            let story = await Story.findById(req.params.id).lean();
 
-        if (!story) {
-            return res.render('error/404');
+            if (!story) {
+                return res.render('error/404');
+            }
+
+            if (story.user != req.user.id) {
+                res.redirect('/stories');
+            } else {
+                story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                    new: true,
+                    runValidators: true
+                });
+                res.redirect('/');
+            }
+        } catch (err) {
+            console.error(err);
+            return res.render('error/500');
         }
+        
+    }
+);
 
-        if (story.user != req.user.id) {
-            res.redirect('/stories');
-        } else {
-            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-                new: true,
-                runValidators: true
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete(
+    '/:id',
+    ensureAuth,
+    async (req, res) => {
+        try {
+            await Story.remove({ _id: req.params.id });
+            res.redirect('/dashboard');
+        } catch (err) {
+            console.error(err);
+            return res.render('error/500');
+        }
+    }
+);
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get(
+    '/user/:userId',
+    ensureAuth,
+    async (req, res) => {
+        try {
+            const stories = await Story.find({
+                user: req.params.userId,
+                status: 'public'
+            })
+                .populate('user')
+                .lean();
+
+            res.render('stories/index', {
+                stories
             });
-            res.redirect('/');
+        } catch (err) {
+            console.error(err);
+            res.render('error/500');
         }
     }
 );
